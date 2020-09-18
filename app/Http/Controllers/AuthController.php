e625f241b4cce895d68e3bf360c042c751238782
javierortegon <javierortegonmp@gmail.com>
2020-09-18 09:37:25 -0500

    creando api rest

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use mysql_xdevapi\Exception;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        try{
+            $user = User::where('email', $request->email)->first();
+
+            if(!$user || !Hash::check($request->password, $user->password)){
+                throw  new Exception('credentials are incorrect', 401);
+            }
+            $token = $user->createToken('Auth Token')->accessToken;
+
+            return response()->json($token, 200);
+
+        }catch (Exception $e){
+            return response()->error($e->getMessage(), $e->getCode());
+        }
+    }
+
+    public function allUsers(){
+        try{
+            $users = User::all();
+            return response()->json($users, 200);
+        }catch (Exception $e){
+            return response()->error($e->getMessage(), $e->getCode());
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/newapi', 'MoviesController@index');
+
+Route::post('user/login', 'AuthController@login');
+
+Route::get('user/all', 'AuthController@allUsers')->middleware('auth:api');
